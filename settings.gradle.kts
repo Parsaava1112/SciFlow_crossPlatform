52931e5533707a0c4e4bdce072bf615650ea438b
Parsa Ebrahimi <eparsa478@gmail.com>
2026-06-17 00:40:23 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,14 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
+}
+
 include(":app")
-project(":app").projectDir = File("android/app")
+project(":app").projectDir = file("android/app")
