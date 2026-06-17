349eab568beb61ea1078ed96bbf94f05f65f1293
Parsa Ebrahimi <eparsa478@gmail.com>
2026-06-17 09:58:15 +0330

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,20 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        // مسیر محلی پلاگین‌های Flutter
+        maven {
+            url = uri("${System.getenv("FLUTTER_ROOT")}/packages/flutter_tools/gradle")
+        }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        // بارگذاری پلاگین فلاتر از مسیر محلی
+        classpath("dev.flutter:flutter-gradle-plugin:1.0.0")
+    }
+}
+
 allprojects {
     repositories {
         google()
